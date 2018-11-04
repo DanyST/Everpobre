e51dfbcec2fed31301baa1dd4c3225c1fc521ffa
Luis Herrera Lillo <dany.herrera20_@hotmail.com>
2018-11-04 06:15:43 -0300

    Add NoteDetailViewController

---
+++ Everpobre/NoteDetail/NoteDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoteDetailViewController.swift
+//  Everpobre
+//
+//  Created by Luis Herrera Lillo on 04-11-18.
+//  Copyright © 2018 Luis Herrera Lillo. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
